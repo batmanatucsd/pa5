132658c5d7f79f4899de4be229f1ac07c62a2dcd
Kenny Yokoyama <kyokoyam@ucsd.edu>
2015-02-12 07:33:59 -0800

    got non-blocking IO working

--- src/motion_mode_keyboard.cpp
+++ src/motion_mode_keyboard.cpp
@@ -5,6 +5,37 @@
 
 #define FOFA 91 // Key code
 #define MOG2 93 // Key code
+#define CTRLC 3
+
+void sig_handler(int sigid) {
+    ROS_INFO("SIG HANDLER");
+}
+
+int kbhit() {
+    struct termios oldttr, newttr;
+    struct timeval timeout;
+    fd_set read_handles;
+    int status;
+
+    // Get current stdin attributes
+    tcgetattr(STDIN_FILENO, &oldttr);
+    tcgetattr(STDIN_FILENO, &newttr);
+    newttr.c_lflag &= ~(ICANON | ECHO | ISIG);
+    // Check stdin (fd 0) for activity
+    FD_ZERO(&read_handles);
+    FD_SET(STDIN_FILENO, &read_handles);
+    // Set timeout to 0
+    timeout.tv_sec = timeout.tv_usec = 0;
+    status = select(STDIN_FILENO + 1, &read_handles, NULL, NULL, &timeout);
+    if(status < 0)
+    {
+        printf("select() failed in kbhit()\n");
+        exit(1);
+    }
+    // Restore terminal settings
+    tcsetattr(STDIN_FILENO, TCSANOW, &newttr);
+    return status;
+}
 
 int main(int argc, char **argv)
 {
@@ -13,20 +44,17 @@ int main(int argc, char **argv)
 
     bool isReady = false;
     bool isFOFA = true;
+    bool keepRunning = true;
     int keycode = 0;
-    struct termios termttr;
     assignment_5::model_msg srv;
 
     ros::ServiceClient client = n.serviceClient<assignment_5::model_msg>("model_switch");
 
     ROS_INFO("Press '[' for FOFA, ']' for MOG2...");
 
-    tcgetattr(STDIN_FILENO, &termttr);
-    termttr.c_lflag &= ~(ICANON | ECHO);
-    tcsetattr(STDIN_FILENO, TCSANOW, &termttr);
 
-    while (1) {
-        keycode = std::getchar();
+    while (keepRunning) {
+        if(kbhit()) { keycode = std::getchar(); }
         //ROS_INFO("KEY PRESSED: %d", keycode);
         switch (keycode) {
             case FOFA:
@@ -45,8 +73,12 @@ int main(int argc, char **argv)
                     ROS_INFO("Requested MOG2!");
                 }
                 break;
+            case CTRLC: keepRunning = false; break;
             default:
-                break;
+                        if (!ros::master::check()) { 
+                            keepRunning = false;
+                        }
+                        break;
         }
 
         if(isReady) {
@@ -59,6 +91,9 @@ int main(int argc, char **argv)
             // print out successful response
         } else {
             // print out unsuccessful response
+        }
+        if (!ros::master::check()) { 
+            keepRunning = false;
         }
     }
 
