4b9d53288e534b1389ef8a614c06d5d0538154af
Kenny Yokoyama <kyokoyam@ucsd.edu>
2015-02-11 19:32:19 -0800

    added some code for MOG2

--- src/motion_detector.cpp
+++ src/motion_detector.cpp
@@ -7,6 +7,7 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/video/tracking.hpp>
+#include <opencv2/video/background_segm.hpp>
 
 #include <cmath>
 
@@ -24,11 +25,10 @@ using namespace cv;
 
 static void drawMotionIntensity(const Mat& flow, Mat& A);
 static void drawOptFlowMap(const Mat& flow, Mat& cflowmap, int step,
-                    double, const Scalar& color);
+        double, const Scalar& color);
 
 class MotionDetector
 {
-    ros::NodeHandle nh;
     image_transport::ImageTransport it;
     image_transport::Publisher image_pub;
     image_transport::Subscriber image_sub;
@@ -44,97 +44,96 @@ class MotionDetector
 
     void callback_crop(const sensor_msgs::ImageConstPtr& msg);
 
-  public:
-    MotionDetector() : it(nh)
+    public:
+    MotionDetector(ros::NodeHandle nh) : it(nh)
     {
-      image_sub = it.subscribe("/camera/image_raw", 1, &MotionDetector::callback_crop, this);
-      image_pub = it.advertise("/camera/image_raw_cropped", 1);
+        image_sub = it.subscribe("/camera/image_raw", 1, &MotionDetector::callback_crop, this);
+        image_pub = it.advertise("/camera/image_raw_cropped", 1);
 
-      //cv::namedWindow(FLOW_WINDOW, cv::WINDOW_AUTOSIZE);
-      algorithm_mode = FOFA; //intializes the algorithm to FOFA
+        //cv::namedWindow(FLOW_WINDOW, cv::WINDOW_AUTOSIZE);
+        algorithm_mode = FOFA; //intializes the algorithm to FOFA
 
     }
 
     bool switch_callback(assignment_5::model_msg::Request &req,
-        assignment_5::model_msg::Response &res)
+            assignment_5::model_msg::Response &res)
     {
-      switch (algorithm_mode = req.model)
-      {
-        case FOFA: ROS_INFO("requested to use FOFA"); 
-            break;
-        case MOG2: ROS_INFO("requested to use MOG2"); 
-            break;
-        default: break;
-      }
+        switch (algorithm_mode = req.model)
+        {
+            case FOFA: ROS_INFO("requested to use FOFA");
+                       break;
+            case MOG2: ROS_INFO("requested to use MOG2");
+                       break;
+            default: break;
+        }
     }
 };
 
 void MotionDetector::callback_crop(const sensor_msgs::ImageConstPtr& msg)
 {
-  cv_bridge::CvImagePtr cv_ptr;
-  try
-  {   //copy the data //TODO maybe change back to copy
-    cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::MONO8);
-  }
-  catch (cv_bridge::Exception& e)
-  {
-    ROS_ERROR("cv_bridge exception: %s", e.what());
-    return;
-  }
+    cv_bridge::CvImagePtr cv_ptr;
+    try
+    {   //copy the data //TODO maybe change back to copy
+        cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::MONO8);
+    }
+    catch (cv_bridge::Exception& e)
+    {
+        ROS_ERROR("cv_bridge exception: %s", e.what());
+        return;
+    }
 
 
-  switch (algorithm_mode)
-  {
-    case FOFA: 
-        if (!prev.empty()){
-            calcOpticalFlowFarneback(prev, cv_ptr->image, uflow, 0.5, 2, 3, 2, 5, 1.1, 0);
+    switch (algorithm_mode)
+    {
+        case FOFA:
+            if (!prev.empty()){
+                calcOpticalFlowFarneback(prev, cv_ptr->image, uflow, 0.5, 2, 3, 2, 5, 1.1, 0);
 
-            cvtColor(prev, cflow, COLOR_GRAY2BGR);
-            uflow.copyTo(flow);
-            //draws arrows over the prev grayscale frames (cflow)
-            drawOptFlowMap(flow, cflow, 16, 1.5, Scalar(0, 255, 0));
-            imshow(FLOW_WINDOW_ARROWS, cflow);
-            
-            cv::Mat intensityMap = Mat::zeros(flow.rows, flow.cols, CV_8U);
-            //populates the intensityMap with the binary image for flow
-            drawMotionIntensity(flow, intensityMap);
-            imshow(FLOW_WINDOW_BINARY, intensityMap);
+                cvtColor(prev, cflow, COLOR_GRAY2BGR);
+                uflow.copyTo(flow);
+                //draws arrows over the prev grayscale frames (cflow)
+                drawOptFlowMap(flow, cflow, 16, 1.5, Scalar(0, 255, 0));
+                imshow(FLOW_WINDOW_ARROWS, cflow);
 
-            cv::waitKey(1);
+                cv::Mat intensityMap = Mat::zeros(flow.rows, flow.cols, CV_8U);
+                //populates the intensityMap with the binary image for flow
+                drawMotionIntensity(flow, intensityMap);
+                imshow(FLOW_WINDOW_BINARY, intensityMap);
 
-        }
-        break;
-    
-    
-    case MOG2:
-        break;
-    
-  }
+                cv::waitKey(1);
 
-/*
-  if (doCrop)
-  {
-        //create the region to crop
-        cv::Rect roi((cv_ptr->image.cols)/2 - CROP_WIDTH/2, (cv_ptr->image.rows)/2 - CROP_HEIGHT/2, CROP_WIDTH, CROP_HEIGHT);
+            }
+            break;
+        case MOG2:
+            bsmog->apply(cv_ptr->image, fg_mask);
+            cv::imshow(FLOW_WINDOW, fg_mask);
+            break;
+    }
 
-        //rewrite the data with the cropped data
-        cv_ptr->image = cv_ptr->image(roi);
-  }
-*/
+    /*
+       if (doCrop)
+       {
+    //create the region to crop
+    cv::Rect roi((cv_ptr->image.cols)/2 - CROP_WIDTH/2, (cv_ptr->image.rows)/2 - CROP_HEIGHT/2, CROP_WIDTH, CROP_HEIGHT);
 
-  prev = cv_ptr->image.clone();
-  image_pub.publish(cv_ptr->toImageMsg());
+    //rewrite the data with the cropped data
+    cv_ptr->image = cv_ptr->image(roi);
+    }
+    */
+
+    prev = cv_ptr->image.clone();
+    image_pub.publish(cv_ptr->toImageMsg());
 }
 
 static void drawOptFlowMap(const Mat& flow, Mat& cflowmap, int step,
-                    double, const Scalar& color)
+        double, const Scalar& color)
 {
     for(int y = 0; y < cflowmap.rows; y += step)
         for(int x = 0; x < cflowmap.cols; x += step)
         {
             const Point2f& fxy = flow.at<Point2f>(y, x);
             line(cflowmap, Point(x,y), Point(cvRound(x+fxy.x), cvRound(y+fxy.y)),
-                 color);
+                    color);
             circle(cflowmap, Point(x,y), 2, color, -1);
         }
 }
@@ -156,8 +155,6 @@ static void drawMotionIntensity(const Mat& flow, Mat& A)
             //std::cout << "x: " << x << std::endl;
             //std::cout << "y: " << y << std::endl;
 
-
-
             //A.data[A.step[0]*i + A.step[1]*j + 0] = sqrt(x*x + y*y);
             if (mag > THRESH_MAG){
                 A.data[A.step[0]*i + A.step[1]*j + 0] = mag;
@@ -167,14 +164,14 @@ static void drawMotionIntensity(const Mat& flow, Mat& A)
 }
 
 int main(int argc, char **argv) {
-  ros::init(argc, argv, "motion_detector");
-  ros::NodeHandle n;
+    ros::init(argc, argv, "motion_detector");
+    ros::NodeHandle n;
 
-  MotionDetector motion_detector;
+    MotionDetector motion_detector(n);
 
-  ros::ServiceServer switch_service = n.advertiseService("model_switch", &MotionDetector::switch_callback, &motion_detector);
-  ros::spin();
+    ros::ServiceServer switch_service = n.advertiseService("model_switch", 
+            &MotionDetector::switch_callback, &motion_detector);
+    ros::spin();
 
-
-  return 0;
+    return 0;
 }
